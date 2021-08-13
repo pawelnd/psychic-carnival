8b9d061ce70546ee18440447972534916be8cd68
pzych <pawel.zych@gmail.com>
2021-08-13 19:06:10 +0200

    Continue as

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -1,31 +1,37 @@
-import { Flex, Button, Box, HStack, Heading, VStack, Badge } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, Flex, Heading, VStack } from '@chakra-ui/react';
+import React, { useMemo } from 'react';
 import { FaFacebook, FaLock } from 'react-icons/all';
-import {useAuth} from "../lib/auth";
+import { useAuth } from '../lib/auth';
 
 export const LoginPage = () => {
-    let {user} = useAuth();
-    console.log(user)
+  let { user, isLoggedIn } = useAuth();
+  const loggedUserComponentPart = useMemo(
+    () => () => {
+      const label = `Kontynuuj jako ${user.name}`;
+      return (
+        <>
+          <Box>LUB</Box>
+          <Box>
+            <Button>{label}</Button>
+          </Box>
+        </>
+      );
+    },
+    user
+  );
   return (
     <Flex direction="column" alignItems={'center'} justifyContent={'center'} margin="20">
       <Box backgroundColor={'primary'} borderRadius={'90'} padding={'5'} color={'white'} margin={'-5'} zIndex={1}>
         <FaLock size={20} />
       </Box>
-      <Box maxW="md" borderWidth="1px" borderRadius="lg" overflow="hidden"
-           backgroundColor={'white'} padding={'10'} minH={'300px'} minW={'350px'}
-           d={'flex'} alignItems={'center'} justifyContent={'center'}
-      >
+      <Box maxW="md" borderWidth="1px" borderRadius="lg" overflow="hidden" backgroundColor={'white'} padding={'10'} minH={'300px'} minW={'350px'} d={'flex'} alignItems={'center'} justifyContent={'center'}>
         <VStack justifyContent={'space-between'} height={'full'} spacing={'7'}>
           <Heading>Logowanie</Heading>
           <hr />
           <Button onClick={() => (window.location.href = '/api/auth/facebook')} size={'md'} variant={'solid'} colorScheme="facebook" leftIcon={<FaFacebook />}>
             Zaloguj przez Facebook
           </Button>
-          <hr />
-            <Box>LUB</Box>
-            <Box>
-                <Button>Kontynuuj jako  <b> {user.name}</b></Button>
-            </Box>
+          {isLoggedIn ? loggedUserComponentPart() : null}
         </VStack>
       </Box>
     </Flex>
